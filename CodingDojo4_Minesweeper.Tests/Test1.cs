216380c2477fe50a82bf5fc1298cbf889eb41bcb
Oberdan Bitencourt Ferreira <oberdan.bitencourt@gmail.com>
2013-08-27 23:49:40 -0300

    Revert "Added extremely simple logic to solve the problem. Known problem when you"
    This reverts commit 9448d80a74fe51eb81b5e7ecd109bad9fda46992.

--- CodingDojo4_Minesweeper.Tests/Test1.cs
+++ CodingDojo4_Minesweeper.Tests/Test1.cs
@@ -41,42 +41,8 @@ namespace CodingDojo4_Minesweeper.Tests
         [Test]
         public void GivenAnFieldWithOneBombInPosition0ShouldReturnCorrectSolution()
         {
-            _minesweeper.AddBombToFieldAt(0);
-            Assert.AreEqual("*100110000000000", _minesweeper.Solve());
-        }
-
-        [Test]
-        public void GivenAnFieldWithOneBombInPosition1ShouldReturnCorrectSolution()
-        {
             _minesweeper.AddBombToFieldAt(1);
-            Assert.AreEqual("1*10111000000000", _minesweeper.Solve());
-        }
-
-        [Test]
-        public void GivenAnFieldWithOneBombInPosition6ShouldReturnCorrectSolution()
-        {
-            /*
-            * 0    1   1   1
-            * 0    1   *   1
-            * 0    1   1   1
-            * 0    0   0   0
-            */
-            _minesweeper.AddBombToFieldAt(6);
-            Assert.AreEqual("011101*101110000", _minesweeper.Solve());
-        }
-
-        [Test]
-        public void GivenAnFieldWithBombInPositions5And6ShouldReturnCorrectSolution()
-        {
-            /*
-            * 1    2   2   1
-            * 1    *   *   1
-            * 1    2   2   1
-            * 0    0   0   0
-            */
-            _minesweeper.AddBombToFieldAt(5);
-            _minesweeper.AddBombToFieldAt(6);
-            Assert.AreEqual("12211**112210000", _minesweeper.Solve());
+            Assert.AreEqual("1000000000000000", _minesweeper.Solve());
         }
 
         [Test]
@@ -86,70 +52,33 @@ namespace CodingDojo4_Minesweeper.Tests
         }
 
         [Test]
-        public void ShouldVerifyIfLengthIsSixteen()
+        public void GivenAnField_ShouldVerify()
         {
-            Assert.AreEqual(16, _minesweeper.Field.Count);
+            Assert.AreEqual("0000000000000000", _minesweeper.Solve());
         }
 
         private class Minesweeper
         {
-            public List<char> Field { get; private set; }
-            private List<int> BombPositions { get; set; }
-            private const int SquareMatrixDimension = 4;
-            private const int SquareMatrixLength = SquareMatrixDimension*SquareMatrixDimension;
 
+            public char[] Field { get; private set; }
+            
             public Minesweeper()
             {
-                BombPositions = new List<int>();
-                Field = "0000000000000000".ToList();
+                Field = "0000000000000000".ToCharArray();
             }
 
             public void AddBombToFieldAt(int index)
             {
                 Field[index] = '*';
-                BombPositions.Add(index);
             }
 
-            
-
-            internal List<char> Solve()
+            internal char[] Solve()
             {
-                /*
-                 * 0    1   2   3
-                 * 4    5   6   7
-                 * 8    9   10  11
-                 * 12   13  14  15
-                 */
-                FindBombs();
-                var result = new string(Field.ToArray());
+                //var row1 = Field.ToString().Substring(0, 4);
+                //var row2 = Field.ToString().Substring(4, 4);
+                //var row3 = Field.ToString().Substring(8, 4);
+                //var row4 = Field.ToString().Substring(12, 4);
                 return Field;
-            }
-
-            private void FindBombs()
-            {
-                foreach (var pos in BombPositions)
-                {
-                    var adjacentPositions = new[]
-                        {
-                            (pos - SquareMatrixDimension) + 1,  //leftTop
-                            pos - SquareMatrixDimension,        //top
-                            (pos - SquareMatrixDimension) - 1,  //rightTop //BUG
-                            pos - 1,                            //left
-                            pos + 1,                            //right
-                            (pos + SquareMatrixDimension) - 1,  //leftDown //BUG
-                            pos + SquareMatrixDimension,        //down
-                            (pos + SquareMatrixDimension) + 1,  //rightDown
-                        };
-                    foreach (var adjacentPosition in adjacentPositions)
-                    {
-                        if (adjacentPosition >= 0 && adjacentPosition <= SquareMatrixLength)
-                        {
-                            var item = Field[adjacentPosition].ToString();
-                            if (!item.Equals("*"))
-                                Field[adjacentPosition] = (Convert.ToInt32(item) + 1).ToString()[0];
-                        }
-                    }
-                }
             }
         }
     }
